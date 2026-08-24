0bdee90d099e961044b53b94726a7e959ae04235
Chip <chip.johansen99@gmail.com>
2026-08-24 12:58:46 -0400

    trying

--- app/main.py
+++ app/main.py
@@ -12,10 +12,10 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "file_path": file_path,
         "status": "success" if errors else "failure",
-        "errors": [format_linter_error(error) for error in errors]
-        
+        "errors": [format_linter_error(error) for error in errors]        
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors) 
+            for file_path, errors in linter_report.items()]
