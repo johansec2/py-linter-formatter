94b98f8346cfff3397ce592101d307ffbe7a7af1
Chip <chip.johansen99@gmail.com>
2026-08-24 13:14:05 -0400

    last time?

--- app/main.py
+++ app/main.py
@@ -8,14 +8,20 @@ def format_linter_error(error: dict) -> dict:
     }
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
+def format_single_linter_file(file_path, errors):
     return {
+        "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "passed" if errors else "failed",
-        "errors": [format_linter_error(error) for error in errors]
+        "status": "failed" if errors else "passed",
     }
 
 
-def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors)
-            for file_path, errors in linter_report.items()]
+def format_linter_report(errors):
+    return [
+        {
+            "errors": [format_linter_error(error) for error in file_errors],
+            "path": file_path,
+            "status": "failed" if file_errors else "passed",
+        }
+        for file_path, file_errors in errors.items()
+    ]
